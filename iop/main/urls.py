1616a13fd82424bb24b3ecf7ec90985da768b896
KacperZa <lacper.zajac765@gmail.com>
2026-03-23 15:57:23 +0100

    zmiana nazwy

--- iop/main/urls.py
+++ iop/main/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
-    path('cars', views.cars, name='cars'),
+    path('cars', views.article, name='cars'),
     path('contact', views.contact, name='contact'),
     path('login', views.login_user, name='login_user'),
     path('register', views.register, name='register_user'),
