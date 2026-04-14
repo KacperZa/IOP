684d5a9442a4138fcebdd281bfa9b0400305e22a
Patryk Bucała <bucalka.bucalka@gmail.com>
2026-04-14 21:31:07 +0200

    odnowienie strony dla wszystkich ogłoszeń

--- iop/news/views.py
+++ iop/news/views.py
@@ -1,10 +1,10 @@
 
-
 from django.shortcuts import render, redirect
 from django.views.generic import DetailView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.db.models import F
 from django.urls import reverse_lazy
+from django.core.paginator import Paginator 
 import uuid
 from django.conf import settings
 from supabase_client import supabase
@@ -13,7 +13,6 @@ from .models import Articles
 from .forms import ArticlesForm
 
 def upload_image(file):
-    # Wysyla plik do supabase i zwraca url
     extension = file.name.split('.')[-1]
     file_name = f"{uuid.uuid4()}.{extension}"
 
@@ -25,11 +24,14 @@ def upload_image(file):
 
     return supabase.storage.from_(settings.SUPABASE_BUCKET).get_public_url(file_name)
 
-
 def news_home(request):
-    news = Articles.objects.all().order_by('-published_at')
+    articles_list = Articles.objects.all().order_by('-published_at')
+    
+    paginator = Paginator(articles_list, 6) 
+    page_number = request.GET.get('page')
+    news = paginator.get_page(page_number)
+    
     return render(request, 'news/index.html', {'news': news})
-
 
 @login_required
 def news_create(request):
@@ -79,6 +81,11 @@ class NewsUpdateView(UpdateView):
         if self.request.method == 'POST':
             kwargs['files'] = self.request.FILES
         return kwargs
+
+    def form_valid(self, form):
+        if 'image' in self.request.FILES:
+            form.instance.image = upload_image(self.request.FILES['image'])
+        return super().form_valid(form)
 
 class NewsDeleteView(DeleteView):
     model = Articles
