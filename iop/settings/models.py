28dc064cb0affd5393fbeb57c55c733a8cbd7834
KacperZa <lacper.zajac765@gmail.com>
2026-05-24 21:21:34 +0200

    Zmiana modelu użytkownika - usunięcie gender i age

--- iop/settings/models.py
+++ iop/settings/models.py
@@ -3,17 +3,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f'Profil {self.user.username}'
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    avatar_url = models.URLField(blank=True)
-    gender = models.CharField(blank = True, max_length=20)
-    age = models.IntegerField(null=True, blank=False)
-
-    def __str__(self):
-        return f'Profil {self.user.username}'
